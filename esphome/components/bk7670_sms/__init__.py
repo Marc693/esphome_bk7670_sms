a35bc6ff70516cdf4b0ea4b594e83bd742b7a378
Marc693 <32340327+Marc693@users.noreply.github.com>
2026-05-21 09:17:04 +0000

    Fix: remove duplicate class declaration

--- esphome/components/bk7670_sms/__init__.py
+++ esphome/components/bk7670_sms/__init__.py
@@ -1,12 +1,5 @@
-from esphome import automation
-import esphome.config_validation as cv
-import esphome.codegen as cg
-
-from .schema import CONFIG_SCHEMA, BK7670SMSComponent
+from .schema import CONFIG_SCHEMA, BK7670SMS
 from .component import to_code
 
-# Déclaration du domaine
 AUTO_LOAD = ["uart", "output"]
 CODEOWNERS = ["@Marc693"]
-
-CONFIG_SCHEMA = CONFIG_SCHEMA
