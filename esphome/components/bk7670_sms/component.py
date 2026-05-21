7b61c4f46161f01c9ed2ad10e765c4e80086b5d7
Marc693 <32340327+Marc693@users.noreply.github.com>
2026-05-21 07:47:45 +0000

    Méthode B : support des IDs de switch

--- esphome/components/bk7670_sms/component.py
+++ esphome/components/bk7670_sms/component.py
@@ -1,31 +1,28 @@
 import esphome.codegen as cg
-import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_UART_ID
+from esphome.components import uart, switch
+from esphome.const import CONF_ID
+
 from .schema import (
-    CONFIG_SCHEMA,
-    CONF_PIN_CODE,
-    CONF_ACL_NUMBERS,
     CONF_GPIO_AD,
     CONF_GPIO_HE,
     CONF_GPIO_HG,
     CONF_GPIO_POWERKEY,
 )
 
-bk7670_sms_ns = cg.esphome_ns.namespace("bk7670_sms")
-BK7670SMSComponent = bk7670_sms_ns.class_("BK7670SMSComponent", cg.Component)
-
-
-CONFIG_SCHEMA = CONFIG_SCHEMA
+BK7670SMSComponent = cg.global_ns.class_("BK7670SMSComponent", cg.Component, uart.UARTDevice)
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID], config[CONF_UART_ID])
+    var = cg.new_Pvariable(config[CONF_ID])
     await cg.register_component(var, config)
-    await cg.register_uart_device(var, config)
+    await uart.register_uart_device(var, config)
 
-    cg.add(var.set_pin_code(config[CONF_PIN_CODE]))
-    cg.add(var.set_acl_numbers(config[CONF_ACL_NUMBERS]))
+    # Méthode B : assignation des IDs de switch
+    ad = await cg.get_variable(config[CONF_GPIO_AD])
+    he = await cg.get_variable(config[CONF_GPIO_HE])
+    hg = await cg.get_variable(config[CONF_GPIO_HG])
+    pwr = await cg.get_variable(config[CONF_GPIO_POWERKEY])
 
-    cg.add(var.set_gpio_ad(await cg.gpio_pin_expression(config[CONF_GPIO_AD])))
-    cg.add(var.set_gpio_he(await cg.gpio_pin_expression(config[CONF_GPIO_HE])))
-    cg.add(var.set_gpio_hg(await cg.gpio_pin_expression(config[CONF_GPIO_HG])))
-    cg.add(var.set_gpio_powerkey(await cg.gpio_pin_expression(config[CONF_GPIO_POWERKEY])))
+    cg.add(var.set_gpio_ad(ad))
+    cg.add(var.set_gpio_he(he))
+    cg.add(var.set_gpio_hg(hg))
+    cg.add(var.set_gpio_powerkey(pwr))
--- esphome/components/bk7670_sms/schema.py
+++ esphome/components/bk7670_sms/schema.py
@@ -1,16 +1,8 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.const import CONF_ID, CONF_UART_ID
-from esphome.components.uart import UARTComponent
-from esphome import pins
+from esphome.components import uart, switch
+from esphome.const import CONF_ID
 
-# On ne doit PAS importer BK7670SMSComponent ici
-# On déclare seulement l'ID
-bk7670_sms_ns = cg.esphome_ns.namespace("bk7670_sms")
-BK7670SMSComponent = bk7670_sms_ns.class_("BK7670SMSComponent", cg.Component)
-
-CONF_PIN_CODE = "pin_code"
-CONF_ACL_NUMBERS = "acl_numbers"
 CONF_GPIO_AD = "gpio_ad"
 CONF_GPIO_HE = "gpio_he"
 CONF_GPIO_HG = "gpio_hg"
@@ -18,13 +10,13 @@ CONF_GPIO_POWERKEY = "gpio_powerkey"
 
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(BK7670SMSComponent),
-    cv.Required(CONF_UART_ID): cv.use_id(UARTComponent),
+    cv.Required("uart_id"): cv.use_id(uart.UARTComponent),
+    cv.Required("pin_code"): cv.string,
+    cv.Required("acl_numbers"): cv.ensure_list(cv.string),
 
-    cv.Required(CONF_PIN_CODE): cv.string,
-    cv.Required(CONF_ACL_NUMBERS): cv.ensure_list(cv.string),
-
-    cv.Required(CONF_GPIO_AD): pins.gpio_output_pin_schema,
-    cv.Required(CONF_GPIO_HE): pins.gpio_output_pin_schema,
-    cv.Required(CONF_GPIO_HG): pins.gpio_output_pin_schema,
-    cv.Required(CONF_GPIO_POWERKEY): pins.gpio_output_pin_schema,
+    # Méthode B : on accepte des IDs de switch
+    cv.Required(CONF_GPIO_AD): cv.use_id(switch.Switch),
+    cv.Required(CONF_GPIO_HE): cv.use_id(switch.Switch),
+    cv.Required(CONF_GPIO_HG): cv.use_id(switch.Switch),
+    cv.Required(CONF_GPIO_POWERKEY): cv.use_id(switch.Switch),
 })
