3a4dabf1ac968e5cd744c2aedce77a03108a9aee
Marc693 <32340327+Marc693@users.noreply.github.com>
2026-05-21 08:34:11 +0000

    Fix: remove duplicate class declaration

--- esphome/components/bk7670_sms/component.py
+++ esphome/components/bk7670_sms/component.py
@@ -1,5 +1,5 @@
 import esphome.codegen as cg
-from esphome.components import uart, switch
+from esphome.components import uart, output
 from esphome.const import CONF_ID
 
 from .schema import (
@@ -11,15 +11,22 @@ from .schema import (
 )
 
 async def to_code(config):
+    # Création de l'instance C++
     var = cg.new_Pvariable(config[CONF_ID])
+
+    # Enregistrement du composant ESPHome
     await cg.register_component(var, config)
+
+    # Enregistrement UART
     await uart.register_uart_device(var, config)
 
+    # Récupération des outputs GPIO
     ad = await cg.get_variable(config[CONF_GPIO_AD])
     he = await cg.get_variable(config[CONF_GPIO_HE])
     hg = await cg.get_variable(config[CONF_GPIO_HG])
     pwr = await cg.get_variable(config[CONF_GPIO_POWERKEY])
 
+    # Appel des setters C++
     cg.add(var.set_gpio_ad(ad))
     cg.add(var.set_gpio_he(he))
     cg.add(var.set_gpio_hg(hg))
--- esphome/components/bk7670_sms/schema.py
+++ esphome/components/bk7670_sms/schema.py
@@ -1,24 +1,29 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
-from esphome.components import uart, switch
+from esphome.components import uart, output
 from esphome.const import CONF_ID
 
+# Namespace du composant
 bk7670_ns = cg.esphome_ns.namespace("bk7670_sms")
 BK7670SMSComponent = bk7670_ns.class_("BK7670SMSComponent", cg.Component, uart.UARTDevice)
 
+# Champs YAML
 CONF_GPIO_AD = "gpio_ad"
 CONF_GPIO_HE = "gpio_he"
 CONF_GPIO_HG = "gpio_hg"
 CONF_GPIO_POWERKEY = "gpio_powerkey"
 
+# Schéma de configuration
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(BK7670SMSComponent),
+
     cv.Required("uart_id"): cv.use_id(uart.UARTComponent),
     cv.Required("pin_code"): cv.string,
     cv.Required("acl_numbers"): cv.ensure_list(cv.string),
 
-    cv.Required(CONF_GPIO_AD): cv.use_id(switch.Switch),
-    cv.Required(CONF_GPIO_HE): cv.use_id(switch.Switch),
-    cv.Required(CONF_GPIO_HG): cv.use_id(switch.Switch),
-    cv.Required(CONF_GPIO_POWERKEY): cv.use_id(switch.Switch),
+    # IMPORTANT : on utilise output.Output (et non switch.Switch)
+    cv.Required(CONF_GPIO_AD): cv.use_id(output.Output),
+    cv.Required(CONF_GPIO_HE): cv.use_id(output.Output),
+    cv.Required(CONF_GPIO_HG): cv.use_id(output.Output),
+    cv.Required(CONF_GPIO_POWERKEY): cv.use_id(output.Output),
 })
