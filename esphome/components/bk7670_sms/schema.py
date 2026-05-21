5c87636b45c90d4ce4da144a927f1bf6bae9ee6f
Marc693 <32340327+Marc693@users.noreply.github.com>
2026-05-21 06:09:46 +0000

    Fix schema imports and validation

--- esphome/components/bk7670_sms/schema.py
+++ esphome/components/bk7670_sms/schema.py
@@ -1,6 +1,11 @@
+import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome.const import CONF_ID, CONF_UART_ID
+from esphome.components.uart import UARTComponent
 from esphome import pins
+
+# Import obligatoire : sinon NameError
+from .component import BK7670SMSComponent
 
 CONF_PIN_CODE = "pin_code"
 CONF_ACL_NUMBERS = "acl_numbers"
@@ -12,8 +17,10 @@ CONF_GPIO_POWERKEY = "gpio_powerkey"
 CONFIG_SCHEMA = cv.Schema({
     cv.GenerateID(): cv.declare_id(BK7670SMSComponent),
     cv.Required(CONF_UART_ID): cv.use_id(UARTComponent),
+
     cv.Required(CONF_PIN_CODE): cv.string,
     cv.Required(CONF_ACL_NUMBERS): cv.ensure_list(cv.string),
+
     cv.Required(CONF_GPIO_AD): pins.gpio_output_pin_schema,
     cv.Required(CONF_GPIO_HE): pins.gpio_output_pin_schema,
     cv.Required(CONF_GPIO_HG): pins.gpio_output_pin_schema,
