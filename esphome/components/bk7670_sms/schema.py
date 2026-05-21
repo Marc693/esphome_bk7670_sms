565dcc628d89b264c4020f68f739a3cce9f934d9
Marc693 <32340327+Marc693@users.noreply.github.com>
2026-05-21 07:59:17 +0000

    Fix: import BK7670SMSComponent in schema.py

--- esphome/components/bk7670_sms/schema.py
+++ esphome/components/bk7670_sms/schema.py
@@ -3,6 +3,9 @@ import esphome.config_validation as cv
 from esphome.components import uart, switch
 from esphome.const import CONF_ID
 
+# IMPORT CRUCIAL
+from .component import BK7670SMSComponent
+
 CONF_GPIO_AD = "gpio_ad"
 CONF_GPIO_HE = "gpio_he"
 CONF_GPIO_HG = "gpio_hg"
@@ -14,7 +17,7 @@ CONFIG_SCHEMA = cv.Schema({
     cv.Required("pin_code"): cv.string,
     cv.Required("acl_numbers"): cv.ensure_list(cv.string),
 
-    # Méthode B : on accepte des IDs de switch
+    # Méthode B : IDs de switch
     cv.Required(CONF_GPIO_AD): cv.use_id(switch.Switch),
     cv.Required(CONF_GPIO_HE): cv.use_id(switch.Switch),
     cv.Required(CONF_GPIO_HG): cv.use_id(switch.Switch),
