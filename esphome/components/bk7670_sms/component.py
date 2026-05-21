751ed30e11b03bc05793a396d15e38e1865709c4
Marc693 <32340327+Marc693@users.noreply.github.com>
2026-05-21 09:27:55 +0000

    Fix: remove duplicate class declaration

--- esphome/components/bk7670_sms/component.py
+++ esphome/components/bk7670_sms/component.py
@@ -11,9 +11,13 @@ from .schema import (
 )
 
 async def to_code(config):
-    var = cg.new_Pvariable(config[CONF_ID])
+    # Récupérer l’UART parent
+    uart_parent = await cg.get_variable(config["uart_id"])
 
-    # UART uniquement
+    # Créer l’objet C++ AVEC le parent UART
+    var = cg.new_Pvariable(config[CONF_ID], uart_parent)
+
+    # Enregistrer comme UARTDevice
     await uart.register_uart_device(var, config)
 
     # Outputs
